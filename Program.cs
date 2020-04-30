8481f3aed5bc4798afb8a174752319079f407dfc
Celvyn <yisusparley@gmail.com>
2020-04-30 08:40:32 -0600

    Objetos del tipo Matriz creados

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace Matrices
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int filas, columnas;
+
+            Matriz A = new Matriz();
+
+            Console.WriteLine("Ingresa el número de filas");
+
+            filas = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingresa el número de columnas");
+
+            columnas = int.Parse(Console.ReadLine());
+
+            A.SetFilas(filas);
+            A.SetColumnas(columnas);
+            A.SetMatriz();
+            A.SumaMatriz();
+            A.VerMatriz();
         }
     }
 }
